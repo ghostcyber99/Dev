909e8469f6584291821a41291c404f6b0eb2652a
ghostcyber99 <98751168+ghostcyber99@users.noreply.github.com>
2022-08-19 00:34:57 +0100

    done with the delete button

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,22 +1,27 @@
 'use strict'
 let tasks = document.querySelector('#tasks')
+let deleteBtn = document.querySelector('.delete');
+
+//add new task 
+
+//saving to local storage 
 
 document.querySelector('.add-list').addEventListener('click', function(){
     let input = document.querySelector('.enter-list').value;
+    // let deleteBtn = document.querySelector('.delete');
 
     if(input.trim()){
         let newItem = document.createElement('div');
-        newItem.innerHTML = `<div class="task">
-                        <div class="content">
+        newItem.innerHTML = `<div class="task" id="edit">
+                        <div class="content" >
                             <input 
                             value='${input}'
                             type="text"
                             class="text"
-                            readonly
                             /> 
                         </div>
                         <div class="actions">
-                            <button class="edit">Edit</button>
+                            <button class="save">Save</button>
                             <button class="delete">Delete</button>
                         </div>
                     </div>`
@@ -25,4 +30,23 @@ document.querySelector('.add-list').addEventListener('click', function(){
     } else{
         alert('Please enter a task')
     }
-})
+
+    //saving the task to local storage
+    document.querySelector('.save').addEventListener('click', function(){
+        console.log('this is a test');
+    })
+
+    //deleting the tasking 
+    document.querySelector('.delete').addEventListener('click', function(){
+        let elem = document.getElementById('edit');
+        elem.parentNode.removeChild(elem);
+        return false;
+    })
+    
+
+});
+
+
+
+    
+
